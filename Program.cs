52eccb50c1e24e11b118fc33a9b37c0523d39b61
M3hmetCiftci <Mehmet.cift27@gmail.com>
2023-11-05 15:37:28 +0300

    Console.App-Sequence

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //<--------------------------------------string Sequens--------------------->
+            //string[] City = { "Adana", "Ankara", "Afyon", "Gaziantep" };
+            //Console.WriteLine(City[0]);
+            //Console.WriteLine(City[1]);
+            //Console.WriteLine(City[2]);
+            //Console.WriteLine(City[3]);
+
+            //<--------------------------------------int Sequens--------------------->
+            //int[]number = { 1, 2, 3 };
+            //Console.WriteLine(number[2]);
+
+
+            //int[]number = { 10,20,30,40,50 };
+            //for (int i = 0; i < 4; i++) 
+            //{
+            //    Console.WriteLine(number[i]);
+            //}
+
+            //<--------------------------------------decision structures--------------------->
+            //int[]number = { 10, 20, 30,40,50,60,70,80 };
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    if (number[i] % 20 == 0 /*&*/ || number[i]%30==0)
+            //    {
+            //        Console.WriteLine(number[i]);
+            //    }
+
+            //}
+            //Console.Read();
+            //<--------------------------------------Length----------------------------------->
+            //string[] City= { "Ankara","Gaziantep","İstanbul","İzmir"};
+            //for (int i = 0; i < City.Length; i++)
+            //{
+            //    Console.WriteLine(City[i]);
+            //}
+            ////Console.WriteLine(City.Length);
+
+
+            //<--------------------------------------Plus Sequens----------------------------------->
+            //int[] Number = { 1, 2, 3 };
+            //int Plus = 0;
+            //for (int i = 0; i < Number.Length; i++)
+            //{
+            //    Plus += Number[i];
+            //}
+            //Console.WriteLine(Plus);
+
+
+            //<--------------------------------------Sort Metod----------------------------------->
+            //int[] Number = { 10, 30, 20, 5,25 };
+            //Array.Sort(Number);
+            //for (int i = 0; i < Number.Length; i++)
+            //{
+            //    Console.WriteLine(Number[i]);
+            //}
+
+            //<--------------------------------------Reverse----------------------------------->
+            //string[] City = { "Adana", "Bursa", "Ankara", "Diyarbakır", "Balıkesir" };
+            ////Array.Reverse(City);
+            //Array.Sort(City);
+            //Array.Reverse(City);
+            //for (int i = 0; i < City.Length; i++)
+            //{
+            //    Console.WriteLine(City[i]);
+            //}
+
+            //<--------------------------------------IndexOf----------------------------------->
+
+            //string[] Ctiy = { "Ankara", "İzmir", "Kütahya", "Balıkesir", "Muğla" };
+            //int sequence;
+            ////Array.Sort(Ctiy); ---- 3 
+            //sequence = Array.IndexOf(Ctiy, "Kütahya");
+            //Console.WriteLine(sequence);
+            //Console.Read();
+
+            //<--------------------------------------Max----------------------------------->
+            //int[] Number = { 9, 3, 5, 6, 8 };
+            //Console.WriteLine(Number.Max());
+
+
+            //<--------------------------------------Min----------------------------------->
+            //int[] Number = { 9, 3, 5, 6, 8 };
+            //Console.WriteLine(Number.Min());
+
+            //<--------------------------------------Max-Min----------------------------------->
+            //int[] Number = { 9, 3, 5, 6, 8 };
+            //Console.WriteLine(Number.Max());
+            //Console.WriteLine(Number.Min());
+
+            //<--------------------------------------Forech----------------------------------->
+
+            //string[] City = { "Ankara", "Gaziantep", "İstanbul", "Adıyaman" };
+            //foreach (string c in City)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+            //<--------------------------------------Forech Loop----------------------------------->
+
+            // int[] Number = { 1, 2, 3, 4, 5, 6, 7, 8, };
+            // int Plus = 0;
+            //foreach (int i in Number)
+            // {
+            //     Plus= Plus+i;
+            // }
+            // Console.WriteLine(Plus);
+
+            //int[] Number = { 1, 2, 3, 4, 5, 6 };
+            //foreach (int n in Number) 
+            //{
+            //    if (n % 2 == 0) 
+            //    {
+            //        Console.WriteLine(n);
+            //    }
+            //}
+            //Console.Read();
+            //<------------- Entering values ​​into the array from the keyboard------------->
+
+           // string[] City = new string[6];
+           //for (int i = 0; i <6; i++)
+           // {
+           //     Console.Write("City: ");
+           //     City[i] = Console.ReadLine();
+           // }
+           // for (int j = 0; j < 6; j++)
+           // {
+                
+           //     Console.WriteLine(City[j]);
+           // }
+           // Console.Read();
+        }
+    }
+}
